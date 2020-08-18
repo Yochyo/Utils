bf25d66fb04f3b425f4eb1a3d5ffe0110854bcc2
Yochyo <herodragon25@gmail.com>
2020-08-18 19:56:11 +0200

    made logger class open

--- src/main/java/de/yochyo/utils/Logger.kt
+++ src/main/java/de/yochyo/utils/Logger.kt
@@ -4,20 +4,20 @@ import java.io.File
 import java.io.PrintWriter
 import java.io.StringWriter
 
-class Logger(dir: String) {
-    private val directory = File("$dir/logs/")
+open class Logger(dir: String) {
+    protected val directory = File("$dir/logs/")
 
     init {
         directory.mkdirs()
     }
 
-    fun log(message: String, filePrefix: String = "logcat") {
+    open fun log(message: String, filePrefix: String = "logcat") {
         val logFile = File(directory, "$filePrefix ${System.currentTimeMillis()}.txt")
         logFile.createNewFile()
         logFile.writeText(message)
     }
 
-    fun log(e: Throwable, info: String = "", filePrefix: String = "logcat") {
+    open fun log(e: Throwable, info: String = "", filePrefix: String = "logcat") {
         val errors = StringWriter()
         e.printStackTrace(PrintWriter(errors))
         log("$info\n$errors", filePrefix)
